43157a7ec47889bcec6f5dd5ab77fd8d1db7617e
Arjun Nair <arjunnair.mec@gmail.com>
2021-05-22 20:21:57 +0530

    Add testing gyro code

--- parallax.js
+++ parallax.js
@@ -3,13 +3,23 @@ function animate() {
     TWEEN.update()
 }
 
+old_log = console.log;
+new_log = document.getElementById("error")
+console.log = function(message) {
+    if (typeof message == "object") {
+        new_log.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : message) + '<br />'
+    } else {
+        new_log.innerHTML += message + '<br />'
+    }
+}
+
 class ParallaxBox {
     LAYER_DEPTH = 10
     constructor(box_id) {
         this.parallax_box = document.getElementById(box_id)
         this.num_images = 0
         this.parallax_box.classList.add('parallax_box')
-        this.isMouseDown = false
+        this.isPressed = false
     }
     addImage(img_src) {
         this.num_images += 1
@@ -32,7 +42,7 @@ class ParallaxBox {
                     this.startAnimation()
                     break;
                 case "mouse":
-                    this.setupMouseEvents()
+                    this.setupInteractiveEvents()
                     break;
                 default:
                     console.log("Invalid init state", action)
@@ -78,35 +88,89 @@ class ParallaxBox {
         this.tween.start()
         animate()
     }
-    setupMouseEvents() {
+    startPoint(xx, yy) {
+        this.mouseStartPoint = [xx, yy]
+        this.parallax_box.style.transition = '0s'
+        this.isPressed = true
+        // console.log("mouse down", e.clientX, e.clientY)
+    }
+    movePoint(xx, yy) {
+        if (this.isPressed) {
+            const dist_x = this.mouseStartPoint[0] - xx
+            const dist_y = this.mouseStartPoint[1] - yy
+            var transform_x = this.img_width / 2 + dist_x
+            var transform_y = this.img_width / 2 + dist_y
+            transform_x = transform_x > this.img_width ? this.img_width : transform_x
+            transform_y = transform_y > this.img_width ? this.img_width : transform_y
+            transform_x = transform_x < 0 ? 0 : transform_x
+            transform_y = transform_y < 0 ? 0 : transform_y
+
+            this.parallax_box.style.perspectiveOrigin = `${transform_x}px ${transform_y}px`;
+        }
+        // console.log("mouse move", e.clientX, e.clientY)
+    }
+    endPoint() {
+        this.isPressed = false
+        const transform_x = this.img_width / 2
+        const transform_y = this.img_width / 2
+        this.parallax_box.style.transition = '0.5s'
+        this.parallax_box.style.perspectiveOrigin = `${transform_x}px ${transform_y}px`;
+    }
+
+    setupInteractiveEvents() {
         window.addEventListener("mousedown", (e) => {
-            this.mouseStartPoint = [e.clientX, e.clientY]
-            this.parallax_box.style.transition = '0s'
-            this.isMouseDown = true
-            // console.log("mouse down", e.clientX, e.clientY)
+            this.startPoint(e.clientX, e.clientY)
         })
         window.addEventListener("mousemove", (e) => {
-            if (this.isMouseDown) {
-                const dist_x = this.mouseStartPoint[0] - e.clientX
-                const dist_y = this.mouseStartPoint[1] - e.clientY
-                var transform_x = this.img_width/2 + dist_x
-                var transform_y = this.img_width/2 + dist_y
-                transform_x = transform_x > this.img_width? this.img_width : transform_x
-                transform_y = transform_y > this.img_width? this.img_width : transform_y
-                transform_x = transform_x < 0? 0 : transform_x
-                transform_y = transform_y < 0? 0 : transform_y
-
-                this.parallax_box.style.perspectiveOrigin = `${transform_x}px ${transform_y}px`;
-            }
-            // console.log("mouse move", e.clientX, e.clientY)
+            this.movePoint(e.clientX, e.clientY)
         })
         window.addEventListener("mouseup", (e) => {
-            this.isMouseDown = false
-            const transform_x = this.img_width / 2
-            const transform_y = this.img_width / 2
-            this.parallax_box.style.transition = '0.5s'
-            this.parallax_box.style.perspectiveOrigin = `${transform_x}px ${transform_y}px`;
+            this.endPoint()
         })
-
+        this.parallax_box.addEventListener("touchstart", (e) => {
+            console.log("got touch start")
+            this.startPoint(e.touches[0].clientX, e.touches[0].clientY)
+        })
+        this.parallax_box.addEventListener("touchmove", (e) => {
+            console.log("got touch move")
+            this.movePoint(e.touches[0].clientX, e.touches[0].clientY)
+            e.preventDefault();
+            e.stopPropagation();
+            return false
+        }, false)
+        this.parallax_box.addEventListener("touchend", (e) => {
+            console.log("got touch end")
+            this.endPoint()
+        })
+        const btn = document.getElementById("request")
+        btn.addEventListener("click", () => {
+            console.log("click gyro")
+            if (typeof (DeviceMotionEvent) !== "undefined" && typeof (DeviceMotionEvent.requestPermission) === "function") {
+                console.log("getting somewhere")
+                DeviceMotionEvent.requestPermission()
+                    .then((response) => {
+                        console.log("got response " + response)
+                        if (response == "granted") {
+                            window.addEventListener("deviceorientation", (e) => {
+                                var absolute = e.absolute;
+                                var alpha = e.alpha;
+                                var beta = e.beta;
+                                var gamma = e.gamma;
+                                document.getElementById("absolute").innerHTML = absolute
+                                document.getElementById("alpha").innerHTML = alpha
+                                document.getElementById("beta").innerHTML = beta
+                                document.getElementById("gamma").innerHTML = gamma
+                                document.getElementById("val").innerHTML = "got val"
+                            }, true);
+                        }
+                        window.addEventListener("deviceorientation", (e) => {
+                            document.getElementById("val").innerHTML = "got second"
+                        })
+                    })
+                    .catch(console.log)
+            } else {
+                alert("DeviceMotionEvent is not defined")
+            }
+        })
     }
 }
