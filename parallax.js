cd9d99f435b6b4c3f8279f4edb99c0364aa39c64
Arjun Nair <arjunnair.mec@gmail.com>
2021-05-31 23:34:08 +0530

    Add more debug data for gyro

--- parallax.js
+++ parallax.js
@@ -140,6 +140,7 @@ class ParallaxBox {
             this.initGyroBeta = beta
             this.initGyroGamma = gamma
             this.isGyroInitialized = true
+            console.log("gyro initialized")
         }
         const MAX_ANGLE = 40
         var transform_x_angle = this.initGyroGamma - gamma
@@ -180,6 +181,7 @@ class ParallaxBox {
             return false
         }, false)
         this.parallax_box.addEventListener("touchend", (e) => {
+            console.log("is touched now false")
             this.is_touched = false
             this.endPoint()
         })
